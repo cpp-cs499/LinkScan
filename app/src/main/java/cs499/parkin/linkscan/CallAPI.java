e71493e88e6ab59a85cb0d4d9647c0af9d74c313
sap172 <saparkin@csupomona.edu>
2016-01-11 22:00:08 -0800

    Trying out POST.

--- app/src/main/java/cs499/parkin/linkscan/CallAPI.java
+++ app/src/main/java/cs499/parkin/linkscan/CallAPI.java
@@ -7,66 +7,72 @@ import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by parkin on 1/11/2016.
  */
-public class CallAPI extends AsyncTask<String, String, String> {
+public class CallAPI extends AsyncTask<URL, String, String> {
 
     @Override
-    protected String doInBackground(String... params) {
-        String urlString=params[0];
-        String resultToDisplay;
-        emailVerificationResult result = null;
-        InputStream in = null;
-
-        // HTTP Get
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            in = new BufferedInputStream(urlConnection.getInputStream());
-        } catch (Exception e ) {
-            System.out.println(e.getMessage());
-            return e.getMessage();
-        }
-
-        // Parse XML
-        XmlPullParserFactory pullParserFactory;
+    protected String doInBackground(URL... url) {
 
         try {
-            pullParserFactory = XmlPullParserFactory.newInstance();
-            XmlPullParser parser = pullParserFactory.newPullParser();
+            // curl_init and url
+            HttpURLConnection con = (HttpURLConnection) url[0].openConnection();
 
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(in, null);
-            result = parseXML(parser);
-        } catch (XmlPullParserException e) {
+            String key = "apikey";
+            String value = "";
+            setupPostConnection(con);
+            setupValues(con, key, value);
+
+
+            DataOutputStream output = new DataOutputStream(con.getOutputStream());
+            output.writeBytes(postData);
+            output.close();
+
+            // "Post data send ... waiting for reply");
+            int code = con.getResponseCode(); // 200 = HTTP_OK
+            System.out.println("Response    (Code):" + code);
+            System.out.println("Response (Message):" + con.getResponseMessage());
+
+            // read the response
+            DataInputStream input = new DataInputStream(con.getInputStream());
+            int c;
+            StringBuilder resultBuf = new StringBuilder();
+            while ((c = input.read()) != -1) {
+                resultBuf.append((char) c);
+            }
+            input.close();
+
+            return resultBuf.toString();
+        }catch(IOException e){
             e.printStackTrace();
-        } catch (IOException e) {
+        }
+
+        return null;
+    }
+
+    protected void setupPostConnection(HttpURLConnection connection){
+        try{
+            connection.setRequestMethod("POST");
+            connection.setInstanceFollowRedirects(true);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+        }catch(IOException e){
             e.printStackTrace();
         }
+    }
+    protected void setupValues(HttpURLConnection connection, String key, String value){
 
-        // Simple logic to determine if the email is dangerous, invalid, or valid
-        if (result != null ) {
-            if( result.hygieneResult.equals("Spam Trap")) {
-                resultToDisplay = "Dangerous email, please correct";
-            }
-            else if( Integer.parseInt(result.statusNbr) >= 300) {
-                resultToDisplay = "Invalid email, please re-enter";
-            }
-            else {
-                resultToDisplay = "Thank you for your submission";
-            }
-        }
-        else {
-            resultToDisplay = "Exception Occured";
-        }
-
-        return resultToDisplay;
+        connection.setRequestProperty(key, value);
     }
 
     protected void onPostExecute(String result) {
